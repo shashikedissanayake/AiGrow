d23a27ae40d35d24beb7907f4503d536bab2eba4
Iranga Navaratna <iranganavaratna@gmail.com>
2017-11-13 10:44:32 +0530

    initial

---
+++ AiGrow.Business/BL_Location.cs
@@ -0,0 +1,17 @@
+﻿using AiGrow.Data;
+
+namespace AiGrow.Business
+{
+    public class BL_Location
+    {
+        public bool insert(AiGrow.Model.ML_Location location)
+        {
+            return new DL_Location().insert(location);
+        }
+
+        public bool delete(AiGrow.Model.ML_Location loaction)
+        {
+            return new DL_Location().delete(loaction);
+        }
+    }
+}
---
+++ AiGrow.DeviceServer/SensorController.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace AiGrow.DeviceServer
+{
+    /// <summary>
+    /// Summary description for SensorController
+    /// </summary>
+    [WebService(Namespace = "http://aigrow.lk/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SensorController : System.Web.Services.WebService
+    {
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+        public string AddDeviceToBayLine(string deviceID, string status)
+        {
+            return "Hello World";
+        }
+    }
+}
